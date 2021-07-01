1c5bc3d5ea292c70ec15f5682214de1288361dd0
신은지 <eunji980310@gmail.com>
2021-07-02 02:34:05 +0900

    chore : Django-MySQL 연동
    close #2

--- FBIproject/FBIproject/settings.py
+++ FBIproject/FBIproject/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'FBIproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'fbi',                    
+        'USER': 'schoolpizza',                         
+        'PASSWORD': 'root1234',                 
+        'HOST': 'localhost',                    
+        'PORT': '3306',      
     }
 }
-
 
 
 # Password validation
